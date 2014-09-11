57c55879fdd630427c7a0e5d471b542b05bb7f13
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2014-09-11 00:05:08 -0500

    First full rough draft version.

--- Layers/Layers/RainforestInfoCardCell.swift
+++ Layers/Layers/RainforestInfoCardCell.swift
@@ -45,10 +45,18 @@ class RainforestInfoCardCell: UICollectionViewCell {
     
     
     // TODO: Add shadow
+//    var shadow = NSShadow()
+//    shadow.shadowColor = UIColor(white: 0.0, alpha: 0.8)
+//    shadow.shadowOffset = CGSize(width: 1, height: 3)
+//    shadow.shadowBlurRadius = 2.0
+    
     descriptionTextView.font = UIFont(name: "AvenirNext-Medium", size: 16)
     descriptionTextView.textAlignment = .Justified
     descriptionTextView.textColor = UIColor.whiteColor()
     descriptionTextView.backgroundColor = UIColor.clearColor()
+    descriptionTextView.scrollEnabled = false
+    
+//    descriptionTextView.textContainer.exclusionPaths = [UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 90, height: 90), byRoundingCorners: UIRectCorner.AllCorners, cornerRadii: CGSize(width: 40, height: 40))]
     
     backgroundGradientLayer = CAGradientLayer()
     backgroundGradientLayer.backgroundColor = UIColor.clearColor().CGColor
@@ -69,32 +77,49 @@ class RainforestInfoCardCell: UICollectionViewCell {
   }
   
   override func sizeThatFits(size: CGSize) -> CGSize {
-    return CGSize(width: 320, height: 500)
+    if let featureImageViewFrame = featureImageViewFrameWithWidth(size.width) {
+      let descriptionTextViewFrame = descriptionTextViewFrameWithWidth(size.width, featureImageViewFrame: featureImageViewFrame)
+      return CGSize(width: size.width, height: descriptionTextViewFrame.maxY + 20.0)
+    }
+    return CGSize(width: size.width, height: 10.0)
   }
+  
   
   override func layoutSubviews() {
     super.layoutSubviews()
     
     backgroundImageView.frame = bounds
     
-    
-    // TODO: Handle scale.
-    let height = (featureImageView.image.size.height / featureImageView.image.size.width) * 320.0
-    featureImageView.frame = CGRectMake(0, 0, 320.0, height)
-    featureImageView.layer.shadowPath = UIBezierPath(rect: featureImageView.bounds).CGPath
+    if let featureImageViewFrame = featureImageViewFrameWithWidth(self.bounds.width) {
+      featureImageView.frame = featureImageViewFrame
+      featureImageView.layer.shadowPath = UIBezierPath(rect: featureImageView.bounds).CGPath
+    } else {
+      featureImageView.frame = CGRectZero
+      featureImageView.layer.shadowPath = UIBezierPath(rect: CGRectZero).CGPath
+    }
     
     backgroundGradientLayer.frame = featureImageView.bounds
     contentView.layer.insertSublayer(backgroundGradientLayer, above: featureImageView.layer)
     
+    titleLabel.frame = CGRectInset(CGRectMake(0, featureImageView.frame.maxY - 70.0, self.bounds.width, 70), 20, 20)
     
-    
-    titleLabel.frame = CGRectInset(CGRectMake(0, featureImageView.frame.size.height - 70.0, 320, 70), 20, 20)
-    
-    let textViewHeight = descriptionTextView.sizeThatFits(CGSize(width: 300, height: DBL_MAX))
-    descriptionTextView.frame = CGRectMake(10, featureImageView.frame.height + 20.0, 300, textViewHeight.height)
-    
+    descriptionTextView.frame = descriptionTextViewFrameWithWidth(self.bounds.width, featureImageViewFrame: featureImageView.frame)
   }
   
   
+  func featureImageViewFrameWithWidth(width: CGFloat) -> CGRect? {
+    // TODO: Handle 1.0 scale.
+    if let image = featureImageView.image {
+      let height = (image.size.height / image.size.width) * width
+      return CGRectMake(0, 0, width, height)
+    }
+    return nil
+  }
   
+  func descriptionTextViewFrameWithWidth(width: CGFloat, featureImageViewFrame: CGRect) -> CGRect {
+    let margin: CGFloat = 20.0
+    let insetWidth: CGFloat = width - margin
+    let textViewHeight = descriptionTextView.sizeThatFits(CGSize(width: insetWidth, height: CGFloat(FLT_MAX)))
+    return CGRectMake(margin/2.0, featureImageViewFrame.height + margin, insetWidth, textViewHeight.height)
+  }
 }
--- Layers/Layers/RainforestViewController.swift
+++ Layers/Layers/RainforestViewController.swift
@@ -13,6 +13,8 @@ class RainforestViewController: UICollectionViewController, UICollectionViewData
                             
   override func viewDidLoad() {
     super.viewDidLoad()
+    let flowLayout = collectionView?.collectionViewLayout as UICollectionViewFlowLayout
+    flowLayout.estimatedItemSize = CGSize(width: 320.0, height: 600.0)
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -21,30 +23,37 @@ class RainforestViewController: UICollectionViewController, UICollectionViewData
     // Dispose of any resources that can be recreated.
   }
   
-  override func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
-    return 10
+  override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 32
   }
   
-  override func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+  // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+  override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     var cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as RainforestInfoCardCell
     
     if indexPath.item == 0 {
-      cell.descriptionTextView.text = "The harpy eagle (Harpia harpyja) is a Neotropical species of eagle. It is sometimes known as the American harpy eagle to distinguish it from the Papuan eagle which is sometimes known as the New Guinea harpy eagle or Papuan harpy eagle.[3] It is the largest and most powerful raptor found in the Americas,[4] and among the largest extant species of eagles in the world. "
-      cell.featureImageView.image = UIImage(named: "harp")
-      cell.backgroundImageView.image = UIImage(named: "harp").applyLightEffect()
+      cell.descriptionTextView.text = "漊煻獌 誙賗跿 藡覶譒 妶岯 蒮 瀤瀪璷 桏毢涒 翐脬 褅, 鑆驈 岋巠帎 胾臷菨 螏螉褩 儇 磝磢磭 蒠蓔蜳 椻楒 斠, 筩 鞮鞢 撖撱暲 譖貚趪 襛襡襙 摲 虙蛃 腠腶舝 玝甿虮, 濍燂犝 珝砯砨 犿玒 褅, 廙彯 滈溔滆 咍垀坽 鄻鎟霣 嗂 嗂 曏樴橉 薽薸蟛 瞵瞷矰 踛輣, 橁橖澭 縢羱聬 奫嫮嫳 漀 憃撊 蜸 墐墆墏 梴棆棎 烍烚珜 鵛嚪 雗雘雝 悊惀桷 甀 裧頖, 踆跾踄 蹸蹪鏂 誙 硻禂 讔雥 烢烒珛 瓥籪艭 鈁陾靰 儇 韎 棎棦 雈靮傿 寱懤擨, 絒翗 腠 痑祣筇 礂簅縭 氉燡磼 獧瞝瞣 蜬蝁蜠 瓂癚 瞂"
+      cell.featureImageView.image = UIImage(named: "a01")
+      cell.backgroundImageView.image = UIImage(named: "a01").applyLightEffect()
       cell.titleLabel.text = "Harpy Eagle"
     } else if indexPath.item == 1 {
-      cell.descriptionTextView.text = "Chameleons or chamaeleons (family Chamaeleonidae) are a distinctive and highly specialized clade of lizards. The approximately 160 species of chameleon come in a range of colors, and many species have the ability to change colors. Chameleons are distinguished by their zygodactylous feet; their very long, highly modified, rapidly extrudable tongues;"
-      cell.featureImageView.image = UIImage(named: "animal")
-      cell.backgroundImageView.image = UIImage(named: "animal").applyLightEffect()
+      cell.descriptionTextView.text = "漊煻獌 誙賗跿 藡覶譒 妶岯 蒮 瀤瀪璷 桏毢涒 翐脬 褅, 鑆驈 岋巠帎 胾臷菨 螏螉褩 儇 磝磢磭 蒠蓔蜳 椻楒 斠, 筩 鞮鞢 撖撱暲 譖貚趪 襛襡襙 摲 虙蛃 腠腶舝 玝甿虮, 濍燂犝 珝砯砨 犿玒 褅, 廙彯 滈溔滆 咍垀坽 鄻鎟霣 嗂 嗂 曏樴橉 薽薸蟛 瞵瞷矰 踛輣, 橁橖澭 縢羱聬 奫嫮嫳 漀 憃撊 蜸 墐墆墏 梴棆棎 烍烚珜 鵛嚪 雗雘雝 悊惀桷 甀 裧頖, 踆跾踄 蹸蹪鏂 誙 硻禂 讔雥 烢烒珛 瓥籪艭 鈁陾靰 儇 韎 棎棦 雈靮傿 寱懤擨, 絒翗 腠 痑祣筇 礂簅縭 氉燡磼 獧瞝瞣 蜬蝁蜠 瓂癚 瞂"
+      cell.featureImageView.image = UIImage(named: "a02")
+      cell.backgroundImageView.image = UIImage(named: "a02").applyLightEffect()
       cell.titleLabel.text = "Chameleon"
-    } else {
-      cell.descriptionTextView.text = "The Menelaus Blue Morpho (Morpho menelaus) is an iridescent tropical butterfly of Central and South America. It has a wing span of 15 cm (5.9 in). The adult drinks juice from rotten fruit with its long proboscis, which is like a sucking tube. The adult males have brighter colours than the females."
-      cell.featureImageView.image = UIImage(named: "blue")
-      cell.backgroundImageView.image = UIImage(named: "blue").applyLightEffect()
+    } else if indexPath.item < 10 {
+      cell.descriptionTextView.text = "漊煻獌 誙賗跿 藡覶譒 妶岯 蒮 瀤瀪璷 桏毢涒 翐脬 褅, 鑆驈 岋巠帎 胾臷菨 螏螉褩 儇 磝磢磭 蒠蓔蜳 椻楒 斠, 筩 鞮鞢 撖撱暲 譖貚趪 襛襡襙 摲 虙蛃 腠腶舝 玝甿虮, 濍燂犝 珝砯砨 犿玒 褅, 廙彯 滈溔滆 咍垀坽 鄻鎟霣 嗂 嗂 曏樴橉 薽薸蟛 瞵瞷矰 踛輣, 橁橖澭 縢羱聬 奫嫮嫳 漀 憃撊 蜸 墐墆墏 梴棆棎 烍烚珜 鵛嚪 雗雘雝 悊惀桷 甀 裧頖, 踆跾踄 蹸蹪鏂 誙 硻禂 讔雥 烢烒珛 瓥籪艭 鈁陾靰 儇 韎 棎棦 雈靮傿 寱懤擨, 絒翗 腠 痑祣筇 礂簅縭 氉燡磼 獧瞝瞣 蜬蝁蜠 瓂癚 瞂"
+      cell.featureImageView.image = UIImage(named: "a0\(indexPath.item)")
+      cell.backgroundImageView.image = UIImage(named: "a0\(indexPath.item)").applyLightEffect()
       cell.titleLabel.text = "Blue Morpho"
+    } else {
+      cell.descriptionTextView.text = "漊煻獌 誙賗跿 藡覶譒 妶岯 蒮 瀤瀪璷 桏毢涒 翐脬 褅, 鑆驈 岋巠帎 胾臷菨 螏螉褩 儇 磝磢磭 蒠蓔蜳 椻楒 斠, 筩 鞮鞢 撖撱暲 譖貚趪 襛襡襙 摲 虙蛃 腠腶舝 玝甿虮, 濍燂犝 珝砯砨 犿玒 褅, 廙彯 滈溔滆 咍垀坽 鄻鎟霣 嗂 嗂 曏樴橉 薽薸蟛 瞵瞷矰 踛輣, 橁橖澭 縢羱聬 奫嫮嫳 漀 憃撊 蜸 墐墆墏 梴棆棎 烍烚珜 鵛嚪 雗雘雝 悊惀桷 甀 裧頖, 踆跾踄 蹸蹪鏂 誙 硻禂 讔雥 烢烒珛 瓥籪艭 鈁陾靰 儇 韎 棎棦 雈靮傿 寱懤擨, 絒翗 腠 痑祣筇 礂簅縭 氉燡磼 獧瞝瞣 蜬蝁蜠 瓂癚 瞂"
+      cell.featureImageView.image = UIImage(named: "a\(indexPath.item)")
+      cell.backgroundImageView.image = UIImage(named: "a\(indexPath.item)").applyLightEffect()
+      cell.titleLabel.text = "Chameleon"
     }
     
+    cell.setNeedsLayout()
     return cell
   }
   
