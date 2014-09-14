43905d02f4fb6da935ca54341ab01c6730dfe4fd
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2014-09-13 19:23:06 -0500

    Adds Async Display Kit

---
+++ Layers/Pods/Pods-Layers-environment.h
@@ -0,0 +1,22 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AsyncDisplayKit
+#define COCOAPODS_POD_AVAILABLE_AsyncDisplayKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.0beta.
+
+// AsyncDisplayKit/no-arc
+#define COCOAPODS_POD_AVAILABLE_AsyncDisplayKit_no_arc
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.0beta.
+
