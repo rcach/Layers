b26260b589e74108c06c3c1f03b15586d704ec4f
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2014-09-14 19:14:27 -0500

    Second pass at Async Display Kit, more refined.

---
+++ Layers/Layers/LAAnimatedDisplayLayer.h
@@ -0,0 +1,13 @@
+//
+//  LAAnimatedDisplayLayer.h
+//  Layers
+//
+//  Created by René Cacheaux on 9/14/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import "_ASDisplayLayer.h"
+
+@interface LAAnimatedDisplayLayer : _ASDisplayLayer
+
+@end
--- Layers/Layers/RainforestInfoCardCell.swift
+++ Layers/Layers/RainforestInfoCardCell.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class RainforestInfoCardCell: UICollectionViewCell {
   var titleLabel: UILabel!
-  var descriptionTextView: UITextView!
-  var featureImageView: UIImageView!
-  var backgroundImageView: UIImageView!
+  
   var backgroundGradientLayer: CAGradientLayer!
   let textAreaHeight: CGFloat = 300.0
   
+  
+  var contentNode: ASDisplayNode!
+  
   var backgroundImageNode: ASImageNode!
+  var featureImageNode: ASImageNode!
+  var descriptionTextNode: ASTextNode!
+  
   
   required init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
@@ -24,27 +28,34 @@ class RainforestInfoCardCell: UICollectionViewCell {
   
   override func awakeFromNib() {
     super.awakeFromNib()
+    
+    contentNode = ASDisplayNode(layerClass: LAAnimatedDisplayLayer.self)
+    contentNode.layerBacked = true
+//    contentNode.layer.opacity = 0.0
+    
     titleLabel = UILabel(frame: CGRectZero)
-    descriptionTextView = UITextView()
-    featureImageView = UIImageView()
-   
-    backgroundImageView = UIImageView()
+    
+    
     backgroundImageNode = ASImageNode()
-    backgroundImageNode.imageModificationBlock = { input in return input.applyLightEffect() }
+    backgroundImageNode.layerBacked = true
+    backgroundImageNode.imageModificationBlock = { input in
+      let tintColor = UIColor(white: 0.5, alpha: 0.3)
+      return input.applyBlurWithRadius(30, tintColor: tintColor, saturationDeltaFactor: 1.8, maskImage: nil)
+    }
+    backgroundImageNode.contentMode = .ScaleAspectFill
     
-    constructViewHierarchy()
+    featureImageNode = ASImageNode()
+    featureImageNode.layerBacked = true
+    featureImageNode.contentMode = UIViewContentMode.ScaleAspectFit
+//    featureImageNode.layer.shadowOffset = CGSizeMake(0, 2)
+//    featureImageNode.layer.shadowOpacity = 0.5
+//    featureImageNode.layer.shadowColor = UIColor.blackColor().CGColor
+//    featureImageNode.layer.shadowRadius = 2
     
-    backgroundImageView.contentMode = .ScaleAspectFill
+    descriptionTextNode = ASTextNode()
+    descriptionTextNode.layerBacked = true
+    descriptionTextNode.backgroundColor = UIColor.clearColor()
     
-    featureImageView.contentMode = UIViewContentMode.ScaleAspectFit
-    featureImageView.layer.shadowOffset = CGSizeMake(0, 2)
-    featureImageView.layer.shadowOpacity = 0.5
-    featureImageView.layer.shadowColor = UIColor.blackColor().CGColor
-    featureImageView.layer.shadowRadius = 2
-    
-    descriptionTextView.textAlignment = .Justified
-    descriptionTextView.backgroundColor = UIColor.clearColor()
-    descriptionTextView.scrollEnabled = false
     
     backgroundGradientLayer = CAGradientLayer()
     backgroundGradientLayer.backgroundColor = UIColor.clearColor().CGColor
@@ -55,14 +66,18 @@ class RainforestInfoCardCell: UICollectionViewCell {
       UIColor(white: 0, alpha: 0)
     ]
     backgroundGradientLayer.colors = arrayColors
+    
+    constructViewHierarchy()
   }
   
   func constructViewHierarchy() {
-//    contentView.addSubview(backgroundImageView)
-    contentView.addSubview(backgroundImageNode.view)
-    contentView.addSubview(featureImageView)
-    contentView.addSubview(titleLabel)
-    contentView.addSubview(descriptionTextView)
+    contentView.layer.addSublayer(contentNode.layer)
+    
+    contentNode.addSubnode(backgroundImageNode)
+    contentNode.addSubnode(featureImageNode)
+    contentNode.addSubnode(descriptionTextNode)
+
+//    contentView.addSubview(titleLabel)
   }
   
   override func sizeThatFits(size: CGSize) -> CGSize {
@@ -76,35 +91,40 @@ class RainforestInfoCardCell: UICollectionViewCell {
   override func layoutSubviews() {
     super.layoutSubviews()
     
-    backgroundImageView.frame = bounds
-    backgroundImageNode.frame = bounds
+    contentNode.frame = contentView.bounds
+    contentNode.shouldRasterizeDescendants = true
     
-    if let featureImageViewFrame = featureImageViewFrameWithWidth(self.bounds.width) {
-      featureImageView.frame = featureImageViewFrame
-      featureImageView.layer.shadowPath = UIBezierPath(rect: featureImageView.bounds).CGPath
+    backgroundImageNode.frame = contentNode.bounds
+    
+    if let featureImageViewFrame = featureImageViewFrameWithWidth(contentNode.bounds.width) {
+      featureImageNode.frame = featureImageViewFrame
+//      featureImageNode.layer.shadowPath = UIBezierPath(rect: featureImageNode.bounds).CGPath
     } else {
-      featureImageView.frame = CGRectZero
-      featureImageView.layer.shadowPath = UIBezierPath(rect: CGRectZero).CGPath
+      featureImageNode.frame = CGRectZero
+//      featureImageNode.layer.shadowPath = UIBezierPath(rect: CGRectZero).CGPath
     }
     
-    backgroundGradientLayer.frame = featureImageView.bounds
-    contentView.layer.insertSublayer(backgroundGradientLayer, above: featureImageView.layer)
+//    backgroundGradientLayer.frame = featureImageNode.bounds
+    contentView.layer.insertSublayer(backgroundGradientLayer, above: featureImageNode.layer)
     
-    titleLabel.frame = CGRectInset(CGRectMake(0, featureImageView.frame.maxY - 80.0, self.bounds.width, 80), 20, 20)
+    titleLabel.frame = CGRectInset(CGRectMake(0, featureImageNode.frame.maxY - 80.0, contentNode.bounds.width, 80), 20, 20)
     
-    descriptionTextView.frame = CGRectMake(10.0, featureImageView.frame.maxY + 20.0, self.bounds.width - 20.0, textAreaHeight)
+    descriptionTextNode.frame = CGRectMake(24.0, featureImageNode.frame.maxY + 20.0, contentNode.bounds.width - 48.0, textAreaHeight)
   }
   
   override func prepareForReuse() {
     super.prepareForReuse()
     // TODO: Cancel expensive operations.
-    backgroundImageNode.hidden = true
+//    backgroundImageNode.hidden = true
+//    featureImageNode.hidden = true
+    contentNode.preventOrCancelDisplay = true
+    contentNode.layer.contents = nil;
   }
   
   
   func featureImageViewFrameWithWidth(width: CGFloat) -> CGRect? {
     // TODO: Handle 1.0 scale.
-    if let image = featureImageView.image {
+    if let image = featureImageNode.image {
       let height = (image.size.height / image.size.width) * width
       return CGRectMake(0, 0, width, height)
     }
@@ -112,6 +132,9 @@ class RainforestInfoCardCell: UICollectionViewCell {
   }
   
   func updateTitle(title: String, andDescription description: NSString) {
+    var paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.alignment = .Justified
+    
     var titleTextShadow = NSShadow()
     titleTextShadow.shadowColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: 0.3)
     titleTextShadow.shadowOffset = CGSize(width: 0, height: 2)
@@ -124,11 +147,11 @@ class RainforestInfoCardCell: UICollectionViewCell {
     descriptionTextShadow.shadowBlurRadius = 3.0
     
     
-    let titleAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Heavy", size: 30), NSForegroundColorAttributeName: UIColor.whiteColor(), NSShadowAttributeName: titleTextShadow]
+    let titleAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Heavy", size: 30), NSForegroundColorAttributeName: UIColor.whiteColor(), NSShadowAttributeName: titleTextShadow, NSParagraphStyleAttributeName: paragraphStyle]
     titleLabel.attributedText = NSAttributedString(string: title, attributes: titleAttributes)
     
-    let descriptionAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 16), NSShadowAttributeName: descriptionTextShadow, NSForegroundColorAttributeName: UIColor.whiteColor(), NSBackgroundColorAttributeName: UIColor.clearColor()]
-    descriptionTextView.attributedText = NSAttributedString(string: description, attributes: descriptionAttributes)
+    let descriptionAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 16), NSShadowAttributeName: descriptionTextShadow, NSForegroundColorAttributeName: UIColor.whiteColor(), NSBackgroundColorAttributeName: UIColor.clearColor(), NSParagraphStyleAttributeName: paragraphStyle]
+    descriptionTextNode.attributedString = NSAttributedString(string: description, attributes: descriptionAttributes)
   }
   
 }
